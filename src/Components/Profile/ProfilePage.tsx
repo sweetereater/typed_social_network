6032c7689048f1c81f4b8f7169c02b34fe64e2ab
sweetereater <sweeter_2093@mail.ru>
2021-09-06 12:47:48 +0300

    ProfilePage modified (from class to functional component with useState) 84

--- src/Components/Profile/ProfilePage.tsx
+++ src/Components/Profile/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import React, { FocusEvent, ChangeEvent } from 'react';
+import React, { FocusEvent, ChangeEvent, useState } from 'react';
 import { ProfileType } from '../../redux/profilePageReducer';
 import s from './ProfilePage.module.css';
 import { InstagramOutlined, FacebookOutlined, YoutubeOutlined } from '@ant-design/icons'
@@ -10,89 +10,74 @@ interface IProfilePagePropsType {
     updateNewStatus: (status: string) => void
 }
 
-class ProfilePage extends React.Component<IProfilePagePropsType>{
+const ProfilePage = (props: IProfilePagePropsType) => {
 
-    state = {
-        editMode: false,
-        status: this.props.status
+    const [editMode, setEditMode] = useState(false);
+    const [status, setStatus] = useState(props.status)
+
+    const setEditModeOn = () => {
+        setEditMode(true)
     }
 
-    setEditModeOn = () => {
-        this.setState({
-            editMode: true
-        })
+    const setEdinModeOff = () => {
+        setEditMode(false)
     }
 
-    setEdinModeOff = () => {
-        this.setState({
-            editMode: false
-        })
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value);
     }
 
-    handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        this.setState({
-            status: e.currentTarget.value
-        })
-    }
-
-    handleNewStatus = (e: FocusEvent<HTMLInputElement>) => {
+    const handleNewStatus = (e: FocusEvent<HTMLInputElement>) => {
         const newText = e.currentTarget.value;
         if (newText) {
-            this.props.updateNewStatus(newText);
+            props.updateNewStatus(newText);
         } else {
-            this.setState({
-                status: this.props.status
-            })
+            setStatus(props.status)
         }
-        this.setEdinModeOff();
+        setEdinModeOff();
     }
 
+    const {
+        aboutMe,
+        fullName,
+        photos,
+        lookingForAJob,
+        lookingForAJobDescription,
+        userId
+    } = props.profile;
 
-    render() {
+    return (
+        <div className={s.currentProfileContainer}>
+            <img className={s.profilePhoto} src={photos.large} alt={fullName} />
 
-        const {
-            aboutMe,
-            fullName,
-            photos,
-            lookingForAJob,
-            lookingForAJobDescription,
-            userId
-        } = this.props.profile;
+            <div className={s.profileInfoContainer}>
+                <div className={s.profileInfo}>
 
-        return (
-            <div className={s.currentProfileContainer}>
-                <img className={s.profilePhoto} src={photos.large} alt={fullName} />
-
-                <div className={s.profileInfoContainer}>
-                    <div className={s.profileInfo}>
-
-                        <div className={s.profileFullName}>
-                            <h1>{fullName}</h1>
-                            <div onDoubleClick={this.setEditModeOn}>
-                                {this.state.editMode && this.props.id === userId ?
-                                    <input onChange={this.handleInputChange} onBlur={this.handleNewStatus} autoFocus value={this.state.status} /> :
-                                    <p> {this.props.status}</p>}
-                            </div>
+                    <div className={s.profileFullName}>
+                        <h1>{fullName}</h1>
+                        <div onDoubleClick={setEditModeOn}>
+                            {editMode && props.id === userId ?
+                                <input onChange={handleInputChange} onBlur={handleNewStatus} autoFocus value={status} /> :
+                                <p> {props.status}</p>}
                         </div>
-
-                        <h3>About: {aboutMe}</h3>
-                        <p>Текущий статус занятости: {lookingForAJob ? "Ищу работу" : "Не ищу работу"}</p>
-                        <p>Описание: {lookingForAJobDescription}</p>
                     </div>
 
-                    <div className={s.profileStatus}></div>
-
-                    <div className={s.userContacts}>
-                        <InstagramOutlined style={{ fontSize: "2rem", cursor: "pointer", }} />
-                        <FacebookOutlined style={{ fontSize: "2rem", marginLeft: "0.5rem", cursor: "pointer", }} />
-                        <YoutubeOutlined style={{ fontSize: "2rem", marginLeft: "0.5rem", cursor: "pointer", }} />
-                    </div>
-
+                    <h3>About: {aboutMe}</h3>
+                    <p>Текущий статус занятости: {lookingForAJob ? "Ищу работу" : "Не ищу работу"}</p>
+                    <p>Описание: {lookingForAJobDescription}</p>
                 </div>
-            </div>
-        )
-    }
 
+                <div className={s.profileStatus}></div>
+
+                <div className={s.userContacts}>
+                    <InstagramOutlined style={{ fontSize: "2rem", cursor: "pointer", }} />
+                    <FacebookOutlined style={{ fontSize: "2rem", marginLeft: "0.5rem", cursor: "pointer", }} />
+                    <YoutubeOutlined style={{ fontSize: "2rem", marginLeft: "0.5rem", cursor: "pointer", }} />
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default ProfilePage;
